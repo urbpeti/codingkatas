512fcf627eb7898a58b84eb878300c26ccdc41ab
Urbanovics Peter <peter.urbanovics@emarsys.com>
2019-03-15 20:12:12 +0100

    Add test do not modify world if it does not contains vowel after 3. character

--- codewars/shorten-your-speech/test/index.test.js
+++ codewars/shorten-your-speech/test/index.test.js
@@ -1,7 +1,11 @@
 const shortenSpeech = require('../');
 
 describe('shortenSpeech', () => {
-  test('should not modify words less then 4 character', () => {
-    expect(shortenSpeech("Cat dog hot low ?")).toBe("Cat dog hot low ?");
+  it('should not modify words less then 4 character', () => {
+    expect(shortenSpeech('Cat dog hot low ?')).toBe('Cat dog hot low ?');
+  });
+
+  it('should not modify words if it does not contains vowel after 3. character', () => {
+    expect(shortenSpeech('Word world tttttt lhkjqrplmnbvx ?')).toBe('Word world tttttt lhkjqrplmnbvx ?');
   });
 });
